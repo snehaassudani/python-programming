19ba4cfdd7b00ac88b3c4f7107a9e7482dbd5879
snehaassudani <sneha.25004913@jainuniversity.ac.in>
2026-09-02 09:11:15 +0530

    Update experiment 3.py

--- experiment 3.py
+++ experiment 3.py
@@ -12,3 +12,18 @@ print("contains python:","python" in s)
 print("concatenation:",s + "programming")
 s2="   hello world    "
 print("trim:",s2.strip())
+
+
+#OUTPUT:
+original string: hello python
+length: 12
+upper case HELLO PYTHON
+lowercase hello python
+character index 6: p
+position in python: 6
+slice: python
+replace: hello world
+hello python
+contains python: True
+concatenation: hello pythonprogramming
+trim: hello world
