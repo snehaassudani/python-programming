8090b99d7d62f956f8fe76b0cfe4ebbaa9b312b8
snehaassudani <sneha.25004913@jainuniversity.ac.in>
2026-09-02 08:35:05 +0530

    Add files via upload

---
+++ experiment 3.py
@@ -0,0 +1,14 @@
+s="hello python"
+print("original string:",s)
+print("length:",len(s))
+print("upper case",s.upper())
+print("lowercase",s.lower())
+print("character index 6:",s[6])
+print("position in python:",s.find("python"))
+print("slice:",s[6:])
+print("replace:",s.replace("python","world"))
+print(s)
+print("contains python:","python" in s)
+print("concatenation:",s + "programming")
+s2="   hello world    "
+print("trim:",s2.strip())
